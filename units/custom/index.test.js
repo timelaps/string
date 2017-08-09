1da8ff7ee57ad225188ff2c24d52e73c9f6340fd
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-08 20:31:59 -0500

    updated custom to be more readable

--- units/custom/index.test.js
+++ units/custom/index.test.js
@@ -3,6 +3,10 @@ var b = require('@timelaps/batterie');
 b.describe('customUnits', function () {
     b.expect(customUnits).toBeFunction();
     b.it('exposes a way to parse strings, looking for custom piece', function (t) {
-        t.expect(customUnits('5whoops', ['oops', 'eek', 'whoops'])).toBe('whoops');
+        t.expect(customUnits('5whoops', [ //
+            'oops', //
+            'eek', //
+            'whoops'
+        ])).toBe('whoops');
     });
 });
