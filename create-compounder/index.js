8122f25fa48e128379766d4fa78171992fdb3fcd
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-10-15 14:45:13 -0500

    udated reduce

--- create-compounder/index.js
+++ create-compounder/index.js
@@ -2,7 +2,7 @@ var words = require('../words');
 var deburr = require('../deburr');
 var reApos = require('../regexp/apos');
 var cacheable = require('@timelaps/fn/cacheable');
-var arrayReduce = require('@timelaps/array/reduce');
+var arrayReduce = require('@timelaps/n/reduce');
 module.exports = function (callback) {
     return cacheable(function (string) {
         return arrayReduce(words(deburr(string).replace(reApos, '')), callback, '');
